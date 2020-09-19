9fbb55aa996a9531a2e3194ddc9cda557320cde4
Gionata Stante <jonny.stante1@gmail.com>
2020-09-19 19:19:09 +0200

    First table and header

--- frontend/LadyBugApp/src/app/app.module.ts
+++ frontend/LadyBugApp/src/app/app.module.ts
@@ -1,8 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccordionModule } from 'primeng/accordion'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabViewModule } from 'primeng/tabview';
+import { PanelModule } from 'primeng/panel';
+import { PanelMenuModule } from 'primeng/panelmenu'
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { SliderModule } from 'primeng/slider';
+import { ToastModule } from 'primeng/toast';
+import { CustomerService } from './customerservice';
 
 @NgModule({
   declarations: [
@@ -10,9 +29,28 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AccordionModule,
+    TabViewModule,
+    PanelModule,
+    PanelMenuModule,
+    MenubarModule,
+    InputTextModule,
+    ButtonModule,
+    ProgressBarModule,
+    CalendarModule,
+    DropdownModule,
+    MultiSelectModule,
+    TableModule,
+    DialogModule,
+    ContextMenuModule,
+    SliderModule,
+    ToastModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CustomerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
