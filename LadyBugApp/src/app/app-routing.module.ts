9f43578d1e1942cb42a04514a31545c60c8dc502
GitPeppe98 <giuseppe.boccuzzi98@gmail.com>
2021-08-02 21:27:19 +0200

    Angular 12.1.4

--- LadyBugApp/src/app/app-routing.module.ts
+++ LadyBugApp/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   { path: 'home', component: HomePageComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
